073ab3d723c3677ac2939ea68f8350525c3788e8
BeateBjoern <117977335+BeateBjoern@users.noreply.github.com>
2023-05-18 15:31:55 +0200

    Fixed update shift + shiftregistration, fully functional

--- Festivalproject.Server/Repository/ShiftRepository.cs
+++ Festivalproject.Server/Repository/ShiftRepository.cs
@@ -45,7 +45,10 @@ namespace Festivalproject.Server.Repository
               .Set(u => u.Name, shiftUpdated.Name)
               .Set(u => u.startTime, shiftUpdated.startTime)
               .Set(u => u.endTime, shiftUpdated.endTime)
-              .Set(u => u.Description, shiftUpdated.Description);
+              .Set(u => u.Description, shiftUpdated.Description)
+              .Set(u => u.Capacity, shiftUpdated.Capacity)
+              .Set(u => u.Duration, shiftUpdated.Duration)
+              .Set(u => u.ShiftType, shiftUpdated.ShiftType);
 
             var result = await collection.UpdateOneAsync(filter, update);
 
